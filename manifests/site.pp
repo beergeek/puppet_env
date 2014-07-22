0af8610e303a23f088ce970c9323e93568ac2b87
beergeek <beergeek@tpg.com.au>
2014-07-22 12:55:02 +1000

    updates

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,8 @@
-node /^vcac-sdl-web-{\d+4}/ {
+node /^vcac-sdl-web-\d{4}/ {
   include role::frontend_webserver
 }
 
-node /^vcac-sdl-db-{\d+4}/ {
+node /^vcac-sdl-db-\d{4}/ {
   include role::backend_dbserver
 }
 
