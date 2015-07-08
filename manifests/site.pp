63783dc86357cefa0129fdc1ba598ca4a23c58f2
beergeek <brett.gray@puppetlabs.com>
2015-07-09 09:43:37 +1000

    updates

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,14 +1,3 @@
-Package {
-  allow_virtual => false,
-}
-
-node /^vcac-sdl-web-\d{4}/ {
-  include roles::frontend_webserver
-}
-
-node /^vcac-sdl-db-\d{4}/ {
-  include roles::backend_dbserver
-}
 
 node default {
 }
