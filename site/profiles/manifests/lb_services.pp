4ea2893825635f096d23f3489cdf443dbfb62abd
beergeek <brett.gray@puppetlabs.com>
2015-07-15 17:04:37 +1000

    lb services

---
+++ site/profiles/manifests/lb_services.pp
@@ -0,0 +1,20 @@
+class profiles::lb_services {
+
+  Firewall {
+    before  => Class['profiles::fw::post'],
+    require => Class['profiles::fw::pre'],
+  }
+
+  include haproxy
+  haproxy::listen { 'puppet00':
+    collect_exported => false,
+    ipaddress        => $::ipaddress_eth1,
+    ports            => '8140',
+  }
+
+  firewall { '105 allow puppet access':
+    port   => [8140],
+    proto  => tcp,
+    action => accept,
+  }
+}
---
+++ site/roles/manifests/load_balancer.pp
@@ -0,0 +1,5 @@
+class roles::load_balancer {
+
+  include profiles::lb_servies
+
+}
