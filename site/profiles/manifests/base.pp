aad64b1b6a731d690af2b27cdece0b9ae60a7c68
beergeek <brett.gray@puppetlabs.com>
2015-10-28 07:58:13 +0900

    windows base

--- site/profiles/manifests/base.pp
+++ site/profiles/manifests/base.pp
@@ -1,48 +1,80 @@
 class profiles::base {
 
-  $sysctl_settings  = hiera('profiles::base::sysctl_settings')
-  $sysctl_defaults  = hiera('profiles::base::sysctl_defaults')
-  $mco_client_array = hiera_array('profiles::base::mco_client_array', undef)
-
   Firewall {
     before  => Class['profiles::fw::post'],
     require => Class['profiles::fw::pre'],
   }
 
-  if $::kernel == 'linux' {
-    class { 'firewall': }
-    class {['profiles::fw::pre','profiles::fw::post']:}
+  case $::kernel {
+    'linux': {
+      $sysctl_settings  = hiera('profiles::base::sysctl_settings')
+      $sysctl_defaults  = hiera('profiles::base::sysctl_defaults')
+      $mco_client_array = hiera_array('profiles::base::mco_client_array', undef)
 
-    create_resources(sysctl,$sysctl_settings, $sysctl_defaults)
-    ensure_packages(['ruby'])
-    ensure_resource('file',['/etc/puppetlabs/facter','/etc/puppetlabs/facter/facts.d'],{ensure => directory, owner => 'root', group => 'root', mode => '0755'})
+      class { 'firewall': }
+      class {['profiles::fw::pre','profiles::fw::post']:}
 
-    # manage time, timezones, and locale
-    class { 'profiles::time_locale': }
+      create_resources(sysctl,$sysctl_settings, $sysctl_defaults)
+      ensure_packages(['ruby'])
+      file { ['/etc/puppetlabs/facter','/etc/puppetlabs/facter/facts.d']:
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+      }
 
-    # manage SSH
-    class { 'profiles::ssh': }
+      # manage time, timezones, and locale
+      class { 'profiles::time_locale': }
 
-    # manage SUDO
-    class { 'profiles::sudo': }
+      # manage SSH
+      class { 'profiles::ssh': }
 
-    # manage logging
-    #class { 'profiles::logging': }
+      # manage SUDO
+      class { 'profiles::sudo': }
 
-    # manage DNS stuff
-    class { 'profiles::dns': }
+      # manage logging
+      #class { 'profiles::logging': }
 
-    if $mco_client_array {
-      $mco_client_array.each |$cert_title| {
-        file { $cert_title:
-          ensure  => file,
-          path    => "/etc/puppetlabs/mcollective/ssl/clients/${cert_title}-public.pem",
-          owner   => 'root',
-          group   => 'root',
-          mode    => '0440',
-          content => file("${::settings::ssldir}/public_keys/${cert_title}.pem",'/dev/null'),
-          notify  => Service['mcollective'],
+      # manage DNS stuff
+      class { 'profiles::dns': }
+
+      if $mco_client_array {
+        $mco_client_array.each |$cert_title| {
+          file { $cert_title:
+            ensure  => file,
+            path    => "/etc/puppetlabs/mcollective/ssl/clients/${cert_title}-public.pem",
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0440',
+            content => file("${::settings::ssldir}/public_keys/${cert_title}.pem",'/dev/null'),
+            notify  => Service['mcollective'],
+          }
         }
+      }
+    }
+    'windows': {
+
+      $wsus_server      = hiera('profiles::base::wsus_server')
+      $wsus_server_port = hiera('profiles::base::wsus_server_port')
+
+      file { ['C:/ProgramData/PuppetLabs/facter','C:/ProgramData/PuppetLabs/facter/facts.d']:
+        ensure => directory,
+      }
+
+      acl { ['C:/ProgramData/PuppetLabs/facter','C:/ProgramData/PuppetLabs/facter/facts.d']:
+        purge                      => false,
+        permissions                => [
+         { identity => 'Administrator', rights => ['full'], perm_type=> 'allow', child_types => 'all', affects => 'all' },
+         { identity => 'Administrators', rights => ['full'], perm_type=> 'allow', child_types => 'all', affects => 'all'}
+        ],
+        owner                      => 'Administrators',
+        group                      => 'Administrator',
+        inherit_parent_permissions => true,
+      }
+
+      # setup wsus client
+      class { 'wsus_client':
+        server_url => "${wsus_server}:${wsus_server_port}",
       }
     }
   }
--- site/profiles/manifests/database_services.pp
+++ site/profiles/manifests/database_services.pp
@@ -3,18 +3,28 @@ class profiles::database_services {
   $db_hash     = hiera_hash('profiles::database_services::db_hash')
   $db_defaults = hiera('profiles::database_services::db_defaults')
 
-  require mysql::server
-  class {'mysql::bindings':
-    php_enable => true,
-  }
+  case $::kernel {
+    'linux': {
+      require mysql::server
+      class {'mysql::bindings':
+        php_enable => true,
+      }
 
-  # create databases
-  create_resources('mysql::db',$db_hash,$db_defaults)
+      # create databases
+      create_resources('mysql::db',$db_hash,$db_defaults)
 
-  # firewall rules
-  firewall { '101 allow mysql access':
-    port   => [3306],
-    proto  => tcp,
-    action => accept,
+      # firewall rules
+      firewall { '101 allow mysql access':
+        port   => [3306],
+        proto  => tcp,
+        action => accept,
+      }
+    }
+    'windows': {
+      require sqlserver
+    }
+    default: {
+      fail("${::kernel} is not a support OS kernel")
+    }
   }
 }
