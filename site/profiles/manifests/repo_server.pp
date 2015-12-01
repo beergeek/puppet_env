c7edb5d3ebc642b2b0d55326904fd848fadfd9f9
beergeek <brett.gray@puppetlabs.com>
2015-12-01 12:57:05 +1100

    better repo management

--- site/profiles/manifests/repo_server.pp
+++ site/profiles/manifests/repo_server.pp
@@ -8,9 +8,20 @@ class profiles::repo_server {
   $repo_data.each |String $repo_name, Hash $repo_hash| {
     @@yumrepo { $repo_name:
       * => $repo_hash,;
-      defaults:
+      default:
         * => $repo_defaults,;
     }
+  }
+
+  # The following is purely for this Vagrant env
+  file { '/var/www/custom':
+    ensure => link,
+    target => '/vagrant/repo/custom',
+  }
+
+  file { '/var/www/repo':
+    ensure => link
+    target => '/vagrant/repo/repo',
   }
 
 }
