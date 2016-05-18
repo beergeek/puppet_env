88b52bb03ffb50b4a3e347ab0e0b5424e7185dae
beergeek <brett.gray@puppetlabs.com>
2016-05-19 08:28:18 +1000

    smarter test

--- site/profile/spec/classes/profile_base_spec.rb
+++ site/profile/spec/classes/profile_base_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 describe 'profile::base' do
 
-  context 'with defaults for all parameters' do
+  context 'as Linux' do
+    let(:facts) {
+      {
+        :kernel => 'Linux',
+      }
+    }
+    it { should contain_class('profile::dns') }
     it { is_expected.to compile.with_all_deps }
   end
 end
