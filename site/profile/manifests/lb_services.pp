6faaa99082aa5f454b72760e6064067c5dd9da6e
beergeek <brett.gray@puppetlabs.com>
2016-06-07 16:43:39 -0700

    more

--- site/profile/manifests/lb_services.pp
+++ site/profile/manifests/lb_services.pp
@@ -9,7 +9,7 @@ class profile::lb_services {
   if $lb_type == 'f5' {
     include profile::lb_services::f5
   } else {
-    include profile::lb_services::ha_proxy
+    include profile::lb_services::haproxy
   }
 
 }
