2fa1172c80411906ba74b651236391b32c723823
beergeek <brett.gray@puppetlabs.com>
2016-09-26 12:50:10 +1000

    test new IIS profile

--- site/profile/manifests/web_services/iis.pp
+++ site/profile/manifests/web_services/iis.pp
@@ -6,68 +6,8 @@ class profile::web_services::iis {
   $lb           = hiera('profile::web_services::iis::lb',true)
   $export_host  = hiera('profile::web_services::iis::export_host', false)
 
-  # Get correct Features for IIS depending on Windows Version
-  case $::kernelmajversion {
-    '6.0','6.1': {
-      $feature_name = [
-        'Web-Server',
-        'Web-WebServer',
-        'Web-Asp-Net',
-        'Web-ISAPI-Ext',
-        'Web-ISAPI-Filter',
-        'NET-Framework',
-        'WAS-NET-Environment',
-        'Web-Http-Redirect',
-        'Web-Filtering',
-        'Web-Mgmt-Console',
-        'Web-Mgmt-Tools'
-      ]
-    }
-    '6.2.','6.3': {
-      $feature_name = [
-        'Web-Server',
-        'Web-WebServer',
-        'Web-Common-Http',
-        'Web-Asp',
-        'Web-Asp-Net45',
-        'Web-ISAPI-Ext',
-        'Web-ISAPI-Filter',
-        'Web-Http-Redirect',
-        'Web-Health',
-        'Web-Http-Logging',
-        'Web-Filtering',
-        'Web-Mgmt-Console',
-        'Web-Mgmt-Tools'
-        ]
-    }
-    default: {
-      fail("You must be running a 19th centery version of Windows")
-    }
-  }
 
-  Iis::Manage_site {
-    require => Windowsfeature[$feature_name],
-  }
-
-  Iis::Manage_app_pool {
-    require => Windowsfeature[$feature_name],
-  }
-
-  # Enable IIS
-  windowsfeature { $feature_name:
-    ensure => present,
-  }
-
-  # disable default website
-  iis::manage_site { 'Default Web Site':
-    ensure    => absent,
-    site_path => 'C:\inetpub\wwwroot',
-    app_pool  => 'Default Web Site',
-  }
-
-  iis::manage_app_pool { 'Default Web Site':
-    ensure => absent,
-  }
+  require ::iis
 
   if $website_hash {
     $website_hash.each |String $site_name, Hash $website| {
@@ -89,6 +29,10 @@ class profile::web_services::iis {
             ensure => present,
             ip     => $::networking['interfaces']['Ethernet 2']['ip'],
           }
+        }
+
+        iis::website { $site_name:
+          ensure => 'Present',
         }
 
         if !defined(Windows_firewall::Exception["HTTP - ${website['port']}"]) {
@@ -117,40 +61,6 @@ class profile::web_services::iis {
           require             => File["/etc/nagios/conf.d/${::fqdn}_service.cfg"],
         }
 
-        iis::manage_app_pool { $site_name:
-          enable_32_bit           => true,
-          managed_runtime_version => 'v4.0',
-        }
-
-        iis::manage_site { $site_name:
-          site_path   => $_docroot,
-          port        => $website['port'],
-          ip_address  => '*',
-          host_header => $site_name,
-          app_pool    => $site_name,
-          before      => File[$_docroot],
-        }
-
-        acl { $_docroot:
-          target                     => $_docroot,
-          purge                      => false,
-          permissions                => [
-            { identity => 'vagrant', rights => ['full'], perm_type=> 'allow', child_types => 'all', affects => 'all' },
-            { identity => 'Administrators', rights => ['full'], perm_type=> 'allow', child_types => 'all', affects => 'all'}
-            ],
-            owner                      => 'vagrant',
-            group                      => 'Administrators',
-            inherit_parent_permissions => true,
-        }
-        file { $_docroot:
-          ensure  => directory,
-          owner   => 'vagrant',
-          group   => 'Administrators',
-          recurse => true,
-          purge   => true,
-          force   => true,
-          source  => "puppet:///iis_files/${site_name}",
-        }
         # Exported load balancer configuration if required
         if $lb {
           @@haproxy::balancermember { "${site_name}-${::fqdn}":
