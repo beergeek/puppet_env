4fdd6e7fa48db8eafc7a025bdcd63aa3904ffa9b
beergeek <brett.gray@puppetlabs.com>
2016-09-26 14:56:16 +1000

    new iis profile

--- site/profile/manifests/web_services/iis.pp
+++ site/profile/manifests/web_services/iis.pp
@@ -2,7 +2,6 @@ class profile::web_services::iis {
 
   # Lookup website information and load balancer
   $website_hash = hiera('profile::web_services::iis::website_hash',undef)
-  $base_docroot = hiera('profile::web_services::iis::base_docroot')
   $lb           = hiera('profile::web_services::iis::lb',true)
   $export_host  = hiera('profile::web_services::iis::export_host', false)
 
@@ -17,7 +16,6 @@ class profile::web_services::iis {
         $_bypass = true
       }
       if $_bypass or ($search_results != 0) {
-        $_docroot = "${base_docroot}\\${website['docroot']}"
 
         if $export_host {
           @@host { $site_name:
@@ -32,43 +30,47 @@ class profile::web_services::iis {
         }
 
         iis::website { $site_name:
-          ensure => 'Present',
+          ensure       => 'Present',
+          binding_info => $website['binding_hash'],
+          website_path => $website['website_path'],
         }
 
-        if !defined(Windows_firewall::Exception["HTTP - ${website['port']}"]) {
-          windows_firewall::exception { "HTTP - ${website['port']}":
-            ensure       => present,
-            direction    => 'in',
-            action       => 'Allow',
-            enabled      => 'yes',
-            protocol     => 'TCP',
-            local_port   => $website['port'],
-            remote_port  => 'any',
-            display_name => 'HTTP - inbound',
-            description  => 'Inbound rule for Interstroodle',
+        website['binding_hash'].each |Hash $binding| {
+          if !defined(Windows_firewall::Exception["HTTP - ${binding['port']}"]) {
+            windows_firewall::exception { "HTTP - ${binding['port']}":
+              ensure       => present,
+              direction    => 'in',
+              action       => 'Allow',
+              enabled      => 'yes',
+              protocol     => 'TCP',
+              local_port   => $binding['port'],
+              remote_port  => 'any',
+              display_name => 'HTTP - inbound',
+              description  => 'Inbound rule for Interstroodle',
+            }
           }
-        }
 
-        # Export monitoring configuration
-        @@nagios_service { "${::fqdn}_http_${site_name}":
-          ensure              => present,
-          use                 => 'generic-service',
-          host_name           => $::fqdn,
-          service_description => "${::fqdn}_http_${site_name}",
-          check_command       => "check_http!${site_name} -I ${networking['interfaces']['Ethernet 2']['ip']} -p ${website['port']} -u http://${site_name}",
-          target              => "/etc/nagios/conf.d/${::fqdn}_service.cfg",
-          notify              => Service['nagios'],
-          require             => File["/etc/nagios/conf.d/${::fqdn}_service.cfg"],
-        }
+          # Exported load balancer configuration if required
+          if $lb {
+            @@haproxy::balancermember { "${site_name}-${::fqdn}":
+              listening_service => $site_name,
+              server_names      => $::fqdn,
+              ipaddresses       => $::networking['interfaces']['Ethernet 2']['ip'],
+              ports             => $binding['port'],
+              options           => 'check',
+            }
+          }
 
-        # Exported load balancer configuration if required
-        if $lb {
-          @@haproxy::balancermember { "${site_name}-${::fqdn}":
-            listening_service => $site_name,
-            server_names      => $::fqdn,
-            ipaddresses       => $::networking['interfaces']['Ethernet 2']['ip'],
-            ports             => $website['port'],
-            options           => 'check',
+          # Export monitoring configuration
+          @@nagios_service { "${::fqdn}_http_${site_name}":
+            ensure              => present,
+            use                 => 'generic-service',
+            host_name           => $::fqdn,
+            service_description => "${::fqdn}_http_${site_name}",
+            check_command       => "check_http!${site_name} -I ${networking['interfaces']['Ethernet 2']['ip']} -p ${binding['port']} -u http://${site_name}",
+            target              => "/etc/nagios/conf.d/${::fqdn}_service.cfg",
+            notify              => Service['nagios'],
+            require             => File["/etc/nagios/conf.d/${::fqdn}_service.cfg"],
           }
         }
       }
