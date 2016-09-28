19ca78542cecda3ff5a08cd949bb26f3dfaa0972
beergeek <brett.gray@puppetlabs.com>
2016-09-28 14:04:18 +1000

    cached catalogs

---
+++ site/profile/manifests/cached.pp
@@ -0,0 +1,24 @@
+class profile::cached (
+  Boolean $use_cached_catalog = true,
+) {
+
+  if $use_cached_catalog {
+    $ensure = 'present'
+  } else {
+    $ensure = 'absent'
+  }
+
+  case $::os['kernel'] {
+    'windows': { $path = 'C:\ProgramData\PuppetLabs\puppet\puppet.conf' }
+    'linux': { $path = '/etc/puppetlabs/puppet/puppet.conf' }
+    default: { fail("You are not using an operating system!") }
+  }
+
+  pe_ini_setting { 'use_cached_catalog':
+    ensure  => $ensure,
+    path    => $path,
+    section => 'agent',
+    setting => 'use_cached_catalog',
+    value   => true,
+  }
+}
