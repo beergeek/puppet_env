77cad0449f3c2d769de599500536d7e85f06409a
beergeek <brett@puppet.com>
2018-08-06 17:52:50 +0800

    Java KS

--- site/profile/manifests/jira_server.pp
+++ site/profile/manifests/jira_server.pp
@@ -165,8 +165,8 @@ class profile::jira_server (
 
     java_ks { 'jira_ks_cert':
       ensure       => present,
-      certificate  => "${jira_data_dir}/key.pem",
-      #private_key  => "${jira_data_dir}/key.pem",
+      certificate  => "${jira_data_dir}/cert.pem",
+      private_key  => "${jira_data_dir}/key.pem",
       storetype    => 'jceks',
       target       => "${jira_data_dir}/jira.jks",
       password     => 'changeit',
