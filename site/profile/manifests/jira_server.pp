a3ff79f7c71531db75ad91f823eea7081b168134
beergeek <brett@puppet.com>
2018-08-07 21:32:40 +0800

    Java KS

--- site/profile/manifests/jira_server.pp
+++ site/profile/manifests/jira_server.pp
@@ -144,6 +144,7 @@ class profile::jira_server (
       password     => 'changeit',
       trustcacerts => true,
       require      => [Java::Oracle['jdk8'],File["${jira_data_dir}/jira.jks"]],
+      notify       => Class['jira'],
     }
   }
 
@@ -172,6 +173,7 @@ class profile::jira_server (
       password     => 'changeit',
       trustcacerts => true,
       require      => [Java::Oracle['jdk8'],File["${jira_data_dir}/jira.jks"]],
+      notify       => Class['jira'],
     }
   }
 }
