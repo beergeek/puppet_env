3aca60d2b171f0cd68791443adeacb0d9097970d
beergeek <brett@puppet.com>
2018-10-08 17:28:44 +0800

    Latest changes from BBS

--- site/profile/manifests/tomcat_services.pp
+++ site/profile/manifests/tomcat_services.pp
@@ -1,8 +1,8 @@
 class profile::tomcat_services (
-  Hash $tomcat_installs,
-  Hash $tomcat_servers,
-  Hash $tomcat_instances,
-  Hash $tomcat_connectors,
+  Hash           $tomcat_installs,
+  Optional[Hash] $tomcat_servers    = undef,
+  Optional[Hash] $tomcat_instances  = undef,
+  Optional[Hash] $tomcat_connectors = undef,
 ) {
 
   class { 'java': }
@@ -13,21 +13,27 @@ class profile::tomcat_services (
     }
   }
 
-  $tomcat_instances.each |String $instance_name, Hash $instance_info| {
-    tomcat::config::instance { $instance_name:
-      * => $instance_info,;
+  if $tomcat_instances {
+    $tomcat_instances.each |String $instance_name, Hash $instance_info| {
+      tomcat::instance { $instance_name:
+        * => $instance_info,;
+      }
     }
   }
 
-  $tomcat_servers.each |String $server_name, Hash $server_info| {
-    tomcat::config::server { $server_name:
-      * => $server_info,
+  if $tomcat_servers {
+    $tomcat_servers.each |String $server_name, Hash $server_info| {
+      tomcat::config::server { $server_name:
+        * => $server_info,
+      }
     }
   }
 
-  $tomcat_connectors.each |String $connector_name, Hash $connector_info| {
-    tomcat::config::server::connector { $connector_name:
-      * => $connector_info,
+  if $tomcat_connectors {
+    $tomcat_connectors.each |String $connector_name, Hash $connector_info| {
+      tomcat::config::server::connector { $connector_name:
+        * => $connector_info,
+      }
     }
   }
 }
