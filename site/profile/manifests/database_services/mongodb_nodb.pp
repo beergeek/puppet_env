66cb000a9a77337521a64a382bf15fc3796b5eda
beergeek <beergeek1679@gmail.com>
2019-04-29 19:07:16 +1000

    ops manager RS

--- site/profile/manifests/database_services/mongodb_nodb.pp
+++ site/profile/manifests/database_services/mongodb_nodb.pp
@@ -1,7 +1,10 @@
 #
 class profile::database_services::mongodb_nodb (
-  Boolean          $enable_firewall = true,
-  Array[String[1]] $firewall_ports  = ['27017'],
+  Boolean              $enable_firewall = true,
+  Array[String[1]]     $firewall_ports  = ['27017'],
+  Stdlib::Absolutepath $base_data_path  = '/data',
+  Stdlib::Absolutepath $db_data_path    = '/data/db',
+  Stdlib::Absolutepath $db_log_path     = '/data/logs',
 ) {
   require mongodb::os
   require mongodb::user
@@ -14,6 +17,36 @@ class profile::database_services::mongodb_nodb (
       proto  => tcp,
       action => accept,
     }
+  }
+
+  file { [$base_data_path, $db_data_path, $db_log_path]:
+    ensure => directory,
+    owner  => 'mongod',
+    group  => 'mongod',
+    mode   => '0750',
+  }
+
+  selinux::fcontext { "set-${db_data_path}-context":
+    ensure   => present,
+    seltype  => 'mongod_var_lib_t',
+    seluser  => 'system_u',
+    pathspec => "${db_data_path}.*",
+    notify   => Exec["selinux-${db_data_path}"],
+  }
+  exec { "selinux-${db_data_path}":
+    command     => "/sbin/restorecon -R -v ${db_data_path}",
+    refreshonly => true,
+  }
+  selinux::fcontext { "set-${db_log_path}-context":
+    ensure   => present,
+    seltype  => 'mongod_log_t',
+    seluser  => 'system_u',
+    pathspec => "${db_log_path}.*",
+    notify   => Exec["selinux-${db_log_path}"],
+  }
+  exec { "selinux-${db_log_path}":
+    command     => "/sbin/restorecon -R -v ${db_log_path}",
+    refreshonly => true,
   }
 
   include mongodb::automation_agent::install
