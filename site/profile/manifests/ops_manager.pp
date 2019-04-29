c8d49e6d2025731378ecfa7e1c85b6292fbb4977
beergeek <beergeek1679@gmail.com>
2019-04-29 14:21:33 +1000

    ops amnager

---
+++ site/profile/manifests/ops_manager.pp
@@ -0,0 +1,21 @@
+#
+class profile::ops_manager (
+  Boolean       $ops_manager_ssl,
+  String[1]     $gen_key_file_content,
+  String[1]     $appsdb_uri,
+  String[1]     $admin_email_addr,
+  Stdlib::Host  $email_hostname,
+  String[1]     $from_email_addr,
+  String[1]     $reply_email_addr,
+) {
+
+  mongodb::ops_manager {
+    ops_manager_ssl       => $ops_manager_ssl,
+    gen_key_file_content  => $gen_key_file_content,
+    appsdb_uri            => $appsdb_uri,
+    admin_email_addr      => $admin_email_addr,
+    email_hostname        => $email_hostname,
+    from_email_addr       => $from_email_addr,
+    reply_email_addr      => $reply_email_addr,
+  }
+}
