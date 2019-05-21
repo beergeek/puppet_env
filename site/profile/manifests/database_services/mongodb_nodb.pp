4f5cbbe6be94edf20285627a90b32e0a85576064
beergeek <beergeek1679@gmail.com>
2019-05-21 12:51:40 +1000

    New data for environments

--- site/profile/manifests/database_services/mongodb_nodb.pp
+++ site/profile/manifests/database_services/mongodb_nodb.pp
@@ -53,7 +53,7 @@ class profile::database_services::mongodb_nodb (
     keyfile_content          => $keyfile_content,
     pem_file_content         => $pem_file_content,
     pem_file_path            => $pem_file_path,
-    pki_dir                  => $pki_dir,
+    pki_pat                  => $pki_path,
     server_keytab_content    => $server_keytab_content,
     server_keytab_path       => $server_keytab_path,
     svc_user                 => $svc_user,
