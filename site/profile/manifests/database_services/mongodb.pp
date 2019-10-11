c75c08e8d1ea907ffa970251c60d8946c44ba644
beergeek <beergeek1679@gmail.com>
2019-10-12 06:33:05 +1100

    testing

--- site/profile/manifests/database_services/mongodb.pp
+++ site/profile/manifests/database_services/mongodb.pp
@@ -1,11 +1,11 @@
 #
 # Remember '/etc/krb5.conf' and '/etc/openldap/ldap.conf'!
 class profile::database_services::mongodb (
-  #Optional[Sensitive[String[1]]] $client_keytab_content,
-  #Optional[Sensitive[String[1]]] $cluster_auth_pem_content,
-  #Optional[Sensitive[String[1]]] $keyfile_content,
-  #Optional[Sensitive[String[1]]] $pem_file_content,
-  #Optional[Sensitive[String[1]]] $server_keytab_content,
+  Optional[Sensitive[String[1]]] $client_keytab_content,
+  Optional[Sensitive[String[1]]] $cluster_auth_pem_content,
+  Optional[Sensitive[String[1]]] $keyfile_content,
+  Optional[Sensitive[String[1]]] $pem_file_content,
+  Optional[Sensitive[String[1]]] $server_keytab_content,
   #Optional[Stdlib::Absolutepath] $ca_file_path,
   #Optional[Stdlib::Absolutepath] $client_keytab_path,
   #Optional[Stdlib::Absolutepath] $cluster_auth_file_path,
