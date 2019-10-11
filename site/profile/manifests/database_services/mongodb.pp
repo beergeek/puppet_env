4cd6f3eb783cbdca3151cfc7cea7ff9053d60e3e
beergeek <beergeek1679@gmail.com>
2019-10-11 16:42:54 +1100

    testing

--- site/profile/manifests/database_services/mongodb.pp
+++ site/profile/manifests/database_services/mongodb.pp
@@ -82,90 +82,90 @@ class profile::database_services::mongodb (
     require profile::ldap
   }
 
-  class { 'mongodb::install':
-    require => Class['mongodb::supporting'],
-  }
-
-  class { 'mongodb::supporting':
-    base_path                => $base_path,
-    ca_cert_pem_content      => $ca_cert_pem_content,
-    ca_file_path             => $ca_file_path,
-    cluster_auth_file_path   => $cluster_auth_file_path,
-    cluster_auth_pem_content => $cluster_auth_pem_content,
-    db_base_path             => $db_base_path,
-    log_path                 => $log_path,
-    keyfile_content          => $keyfile_content,
-    pem_file_content         => $pem_file_content,
-    pem_file_path            => $pem_file_path,
-    pki_path                 => $pki_path,
-    server_keytab_content    => $server_keytab_content,
-    server_keytab_path       => $server_keytab_path,
-    svc_user                 => $svc_user,
-  }
-
-  $mongod_instance.each |String $instance_name, Hash $instance_data| {
-
-    if $instance_data['enable_kerberos'] {
-      require profile::kerberos
-    }
-
-    if $instance_data['enable_ldap_authn'] {
-      require profile::ldap
-    }
-
-    if $instance_data['enable_ldap_authz'] {
-      require profile::ldap
-    }
-
-    mongodb::config { $instance_name:
-      *                  => $instance_data,
-      ldap_bind_password => $ldap_bind_password,
-      before             => Mongodb::Service[$instance_name],
-      require            => Class['mongodb::install'],
-    }
-
-    if $enable_firewall {
-      if $instance_data['port'] {
-        $_port = $instance_data['port']
-      } else {
-        $_port = '27017'
-      }
-      # firewall rules
-      firewall { "101 allow mongodb ${instance_name} access":
-        dport  => [$_port],
-        proto  => tcp,
-        action => accept,
-      }
-    }
-
-    if $instance_data['spn'] {
-      if $instance_data['svc_account'] {
-        @@dsc_xadserviceprincipalname { $instance_data['spn']:
-          ensure      => present,
-          dsc_account => $instance_data['svc_account'],
-        }
-      } else {
-        fail('A service account is required to create a SPN')
-      }
-    }
-
-    mongodb::service { $instance_name:
-      require => Class['mongodb::supporting'],
-    }
-  }
-
-  if $install_aa {
-    class { 'mongodb::automation_agent':
-      ops_manager_fqdn => $ops_manager_fqdn,
-      url_svc_type     => $url_svc_type,
-      mms_group_id     => $mms_group_id,
-      mms_api_key      => $mms_api_key,
-      enable_ssl       => $enable_ssl,
-      ca_file_path     => $aa_ca_file_path,
-      pem_file_path    => $aa_pem_file_path,
-      pem_file_content => $aa_pem_file_content,
-      ca_file_content  => $aa_ca_cert_content,
-    }
-  }
+  #class { 'mongodb::install':
+  #  require => Class['mongodb::supporting'],
+  #}
+#
+  #class { 'mongodb::supporting':
+  #  base_path                => $base_path,
+  #  ca_cert_pem_content      => $ca_cert_pem_content,
+  #  ca_file_path             => $ca_file_path,
+  #  cluster_auth_file_path   => $cluster_auth_file_path,
+  #  cluster_auth_pem_content => $cluster_auth_pem_content,
+  #  db_base_path             => $db_base_path,
+  #  log_path                 => $log_path,
+  #  keyfile_content          => $keyfile_content,
+  #  pem_file_content         => $pem_file_content,
+  #  pem_file_path            => $pem_file_path,
+  #  pki_path                 => $pki_path,
+  #  server_keytab_content    => $server_keytab_content,
+  #  server_keytab_path       => $server_keytab_path,
+  #  svc_user                 => $svc_user,
+  #}
+#
+  #$mongod_instance.each |String $instance_name, Hash $instance_data| {
+#
+  #  if $instance_data['enable_kerberos'] {
+  #    require profile::kerberos
+  #  }
+#
+  #  if $instance_data['enable_ldap_authn'] {
+  #    require profile::ldap
+  #  }
+#
+  #  if $instance_data['enable_ldap_authz'] {
+  #    require profile::ldap
+  #  }
+#
+  #  mongodb::config { $instance_name:
+  #    *                  => $instance_data,
+  #    ldap_bind_password => $ldap_bind_password,
+  #    before             => Mongodb::Service[$instance_name],
+  #    require            => Class['mongodb::install'],
+  #  }
+#
+  #  if $enable_firewall {
+  #    if $instance_data['port'] {
+  #      $_port = $instance_data['port']
+  #    } else {
+  #      $_port = '27017'
+  #    }
+  #    # firewall rules
+  #    firewall { "101 allow mongodb ${instance_name} access":
+  #      dport  => [$_port],
+  #      proto  => tcp,
+  #      action => accept,
+  #    }
+  #  }
+#
+  #  if $instance_data['spn'] {
+  #    if $instance_data['svc_account'] {
+  #      @@dsc_xadserviceprincipalname { $instance_data['spn']:
+  #        ensure      => present,
+  #        dsc_account => $instance_data['svc_account'],
+  #      }
+  #    } else {
+  #      fail('A service account is required to create a SPN')
+  #    }
+  #  }
+#
+  #  mongodb::service { $instance_name:
+  #    require => Class['mongodb::supporting'],
+  #  }
+  #}
+#
+  #if $install_aa {
+  #  class { 'mongodb::automation_agent':
+  #    ops_manager_fqdn => $ops_manager_fqdn,
+  #    url_svc_type     => $url_svc_type,
+  #    mms_group_id     => $mms_group_id,
+  #    mms_api_key      => $mms_api_key,
+  #    enable_ssl       => $enable_ssl,
+  #    ca_file_path     => $aa_ca_file_path,
+  #    pem_file_path    => $aa_pem_file_path,
+  #    pem_file_content => $aa_pem_file_content,
+  #    ca_file_content  => $aa_ca_cert_content,
+  #  }
+  #}
 
 }
