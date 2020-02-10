86e99050ffa44f2ed029999b7bb567afd1fa7ee1
beergeek <beergeek1679@gmail.com>
2020-02-11 09:56:14 +1100

    New AD users

--- site/profile/manifests/ad_services.pp
+++ site/profile/manifests/ad_services.pp
@@ -2,7 +2,23 @@
 class profile::ad_services (
   Hash    $domain_controller_hash,
   Hash    $dns_server_hash,
-  Boolean $collect_hosts          = true,
+  Boolean $collect_hosts             = true,
+  Optional[Hash[
+    String[1],
+    Struct[{
+      password                       => Sensitive.new(String[1]),
+      Optional[ensure]               => Enum['absent','present'],
+      Optional[enabled]              => Boolean,
+      Optional[passwordneverexpires] => Boolean,
+      Optional[cannotchangepassword] => Boolean,
+    }]
+  ]]      $users                     = undef,
+  Struct[{
+    ensure                           => Enum['absent','present'],
+    enabled                          => Boolean,
+    passwordneverexpires             => Boolean,
+    cannotchangepassword             => Boolean,
+  }]      $user_defaults,
 ) {
 
   class { 'active_directory::domain_controller':
@@ -16,6 +32,23 @@ class profile::ad_services (
   # Collection DNS entries from Non-Windows nodes and instantiate
   if $collect_hosts {
     Dsc_xdnsrecord <<| |>>
+  }
+
+  if $users {
+    $users.each |String $username, Hash $user_data| {
+      dsc_xaduser { $username:
+        dsc_ensure               => $user_data['ensure'],
+        dsc_username             => $username,
+        dsc_domain               => $domain_controller_hash['domain_name'],
+        dsc_password             => {
+          'user'     => $username,
+          'password' => $user_data['password']
+        },
+        dsc_enabled              => $user_data['enabled'],
+        dsc_passwordneverexpires => $user_data['passwordneverexpires'],
+        dsc_cannotchangepassword => $user_data['cannotchangepassword'],
+      }
+    }
   }
 
 }
